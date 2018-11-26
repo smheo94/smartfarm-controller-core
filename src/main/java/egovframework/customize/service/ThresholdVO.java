fe31b1e04df5a8f6551c2f0b1209760e1dfd7795
Lee <rami9014@vivans.net>
2018-11-26 15:59:39 +0900

    제어기 임계치 설정 수정, 제어로직 ordering 수정

--- src/main/java/egovframework/customize/service/ThresholdVO.java
+++ src/main/java/egovframework/customize/service/ThresholdVO.java
@@ -18,46 +18,47 @@ import lombok.Setter;
 public class ThresholdVO {
 	// gsmKey
 	Integer gsmKey;
-	// 수정일자
+	// 수정일자	
 	String updateDate;
 	// 외부고온
-	Double outsideHighTemp;	
+	Double stormLimit;	
 	// 외부저온
-	Double outsideLowTemp;
-	// 외부고습
-	Double outsideHighHum;
-	// 외부저습
-	Double outsideLowHum;
-	// 풍속
-	Integer windSpeed;
-	// 감우감지
-	Integer rainDetector;
-	// 내부고온
-	Double insideHighTemp;
-	// 내부저온
-	Double insideLowTemp;
-	// 내부고습
-	Double insideHighHum;
-	// 내부저습
-	Double insideLowHum;
-	// 외부고온사용
-	Integer outsideHighTempUse;
-	// 외부저온사용
-	Integer outsideLowTempUse;
-	// 외부고습사용
-	Integer outsideHighHumUse;
-	// 외부저습사용
-	Integer outsideLowHumUse;
-	//풍속사용
-	Integer windSpeedUse;
-	// 감우갖미 사용
-	Integer rainDetectorUse;
-	// 내부고온사용
-	Integer insideHighTempUse;
-	// 내부저온사용
-	Integer insideLowTempUse;
-	// 내부고습사용
-	Integer insideHighHumUse;
-	// 내부저습사용
-	Integer insideLowHumUse;
+	Double stormSensingDelay;
+	// 
+	Double stormTerminateDelay;
+	// 풍향 대기 시간
+	Double windDirectionWaitingTime;
+	// 풍향 각도
+	Double windDirectionDegree;
+	// 감우 감지 딜레이
+	Double rainSensingDelay;
+	// 감우 종료 딜레이
+	Double rainTerminateDelay;
+	// 좌천창 각도
+	Double topWindowLeftDegree;
+	// 우천창 각도
+	Double topWindowRightDegree;
+	// 센서 선택
+	Double sensorSelect;
+	// 바람 측정 여부
+	Integer useWindDetector;
+	// 감우 측정 여부
+	Integer useRainDetector;
+	// 환기창 바람 측정 여부
+	Integer exWindowUseWindDetector;
+	// 환기창 감우 측정 여부
+	Integer exWindowUseRainDetector;
+	// 측창 바람 측정 여부
+	Integer sideWindowUseWindDetector;
+	// 측창감우측정여부
+	Integer sideWindowUseRainDetector;
+	//천창
+	Integer baseForwardWindow;
+	// 구동기 초기화 온도
+	Integer actuatorInitTemp;
+	// 기상대 mqtt host
+	String weatherMqttHost;
+	// 기상대 mqtt port
+	Integer weatherMqttPort;
+
 }
