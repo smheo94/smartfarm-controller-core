3d642f0ea987aa44e468ab5c64d48b8e9fa4cf60
Eric.Hwang <syhwang@vivans.net>
2019-08-28 19:32:39 +0900

    기능 추가 : 근권부 모니터링 API 추가
    # KTFG-219

--- src/main/java/com/kt/smartfarm/config/RestAuthenticationEntryPoint.java
+++ src/main/java/com/kt/smartfarm/config/RestAuthenticationEntryPoint.java
@@ -5,14 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
-import sun.misc.CharacterEncoder;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.Calendar;
 import java.util.HashMap;
