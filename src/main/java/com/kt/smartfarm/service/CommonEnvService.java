a0122961a8414403fd732fe890301fc02d5f534e
Eric.Hwang <syhwang@vivans.net>
2019-11-14 18:14:22 +0900

    기능 수정 : 센싱 데이터 device_type에 있는 값으로 사용, Sunrize 값 변경

--- src/main/java/com/kt/smartfarm/service/CommonEnvService.java
+++ src/main/java/com/kt/smartfarm/service/CommonEnvService.java
@@ -1,35 +1,35 @@
-package com.kt.smartfarm.service;
-
-import java.util.HashMap;
-import java.util.List;
-
-public interface CommonEnvService {
-	CommonEnvVO getOutWeather(int gsmKey);
-	CommonEnvVO getOutWeatherDefault(int gsmKey);
-	void updateOutWeather(CommonEnvVO vo);	
-	void updateOutWeatherDefault(CommonEnvVO vo);
-	
-	CommonEnvVO getThreshold(int gsmKey);
-	CommonEnvVO getThresholdDefault(int gsmKey);
-	void updateThreshold(CommonEnvVO vo);	
-	void updateThresholdDefault(CommonEnvVO vo);
-	
-	void updateEnvInfo(CommonEnvVO vo);
-	CommonEnvVO getOutweatherInfo(HashMap<String, Object> param);
-	
-//	HashMap<String, Object> getSensorInfoList();
-//	HashMap<String, Object> getDeviceInfoList();
-	HashMap<String, Object> insertDeviceList(HashMap<String, Object> actuatorModuleInfo, String contextPath);	
-	HashMap<String, Object> updateDeviceList(HashMap<String, Object> actuatorModuleInfo, String contextPath);
-	HashMap<String, Object> deleteControllerInfo(String controllerId, String contextPath);
-	
-	HashMap<String, Object> upsertSensorList(HashMap<String, Object> sensorModuleInfo, String contextPath, boolean isNew);
-	HashMap<String, Object> deleteSensorList(String sensorControllerId, String contextPath);
-	List<HashMap<String, Object>> selectedControllerInfo(String selectedControllerId, Integer controllerType);
-	HashMap<String, Object> selectSensorList(Integer sensorType);
-	
-	List<HashMap<String, Object>> getControllerListOfControlModule(int[] controllerInfoId);
-	List<HashMap<String, Object>> selectDeviceOfControlModule(Integer controllerId);
-	
-	
-}
+//package com.kt.smartfarm.service;
+//
+//import java.util.HashMap;
+//import java.util.List;
+//
+//public interface CommonEnvService {
+//	CommonEnvVO getOutWeather(int gsmKey);
+//	CommonEnvVO getOutWeatherDefault(int gsmKey);
+//	void updateOutWeather(CommonEnvVO vo);
+//	void updateOutWeatherDefault(CommonEnvVO vo);
+//
+//	CommonEnvVO getThreshold(int gsmKey);
+//	CommonEnvVO getThresholdDefault(int gsmKey);
+//	void updateThreshold(CommonEnvVO vo);
+//	void updateThresholdDefault(CommonEnvVO vo);
+//
+//	void updateEnvInfo(CommonEnvVO vo);
+//	CommonEnvVO getOutweatherInfo(HashMap<String, Object> param);
+//
+////	HashMap<String, Object> getSensorInfoList();
+////	HashMap<String, Object> getDeviceInfoList();
+//	HashMap<String, Object> insertDeviceList(HashMap<String, Object> actuatorModuleInfo, String contextPath);
+//	HashMap<String, Object> updateDeviceList(HashMap<String, Object> actuatorModuleInfo, String contextPath);
+////	HashMap<String, Object> deleteControllerInfo(String controllerId, String contextPath);
+//
+////	HashMap<String, Object> upsertSensorList(HashMap<String, Object> sensorModuleInfo, String contextPath, boolean isNew);
+////	HashMap<String, Object> deleteSensorList(String sensorControllerId, String contextPath);
+////	List<HashMap<String, Object>> selectedControllerInfo(String selectedControllerId, Integer controllerType);
+////	HashMap<String, Object> selectSensorList(Integer sensorType);
+//
+////	List<HashMap<String, Object>> getControllerListOfControlModule(int[] controllerInfoId);
+////	List<HashMap<String, Object>> selectDeviceOfControlModule(Integer controllerId);
+//
+//
+//}
