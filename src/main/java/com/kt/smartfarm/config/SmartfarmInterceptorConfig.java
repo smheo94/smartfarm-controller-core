33546948ada56f72a77953f16ff74b3b43057fc0
syhwang@vivans.net <syhwang@vivans.net>
2020-03-07 19:48:04 +0900

    wevconfigmvc추가

--- src/main/java/com/kt/smartfarm/config/SmartfarmInterceptorConfig.java
+++ src/main/java/com/kt/smartfarm/config/SmartfarmInterceptorConfig.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -17,6 +18,7 @@ import javax.annotation.Resource;
 
 @Configuration
 @Slf4j
+@EnableWebMvc
 //@PropertySource("classpath:smartfarm-mgr-env.properties")
 //@PropertySource(value={"classpath:application.properties","classpath:smartfarm-mgr-env.properties"}, ignoreResourceNotFound=true)
 @PropertySource(value={"classpath:application.properties","file:/myapp/application.properties","file:/home/gsm/v4/conf/smartfarm-mgr-env.properties"}, ignoreResourceNotFound=true)
