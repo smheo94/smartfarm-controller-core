c27152419d2fdfb62db438f7dfa727010d15d3bf
syhwang@vivans.net <syhwang@vivans.net>
2020-05-28 17:31:42 +0900

    코드 정리 / MAP용 Deserialize 추가

---
+++ src/main/java/com/kt/cmmn/util/MapJsonDeserializer.java
@@ -0,0 +1,24 @@
+package com.kt.cmmn.util;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+public class MapJsonDeserializer extends JsonDeserializer<HashMap<String,Object>> {
+
+
+    @Override
+    public HashMap<String, Object> deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
+        try {
+            HashMap<String, Object> map = new HashMap<>();
+            map = super.deserialize(jsonParser,deserializationContext, map);
+            return map;
+        } catch(java.lang.UnsupportedOperationException e) {
+            return (HashMap<String, Object>) MapUtils.fromJson(jsonParser.getText());
+        }
+    }
+}
--- src/main/java/com/kt/smartfarm/service/HouseEnvVO.java
+++ src/main/java/com/kt/smartfarm/service/HouseEnvVO.java
@@ -1,5 +1,7 @@
 package com.kt.smartfarm.service;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.kt.cmmn.util.MapJsonDeserializer;
 import com.kt.cmmn.util.MapUtils;
 import lombok.Data;
 import lombok.Getter;
@@ -47,11 +49,13 @@ public class HouseEnvVO {
 	Double selectHumSensor;
 	Long gsmKey;
 	String houseNameI18n;
-	HashMap<String,Object> properties = new HashMap<>();
 
-	public void setProperties(String properties) {
-		this.properties = (HashMap<String,Object>)MapUtils.fromJson(properties);
-	}
-	
+	@JsonDeserialize(using = MapJsonDeserializer.class)
+	HashMap<String,Object> properties = new HashMap<>();
+//
+//	public void setProperties(String properties) {
+//		this.properties = (HashMap<String,Object>)MapUtils.fromJson(properties);
+//	}
+//
 	List<CCTVSettingVO> cctv;
 }
