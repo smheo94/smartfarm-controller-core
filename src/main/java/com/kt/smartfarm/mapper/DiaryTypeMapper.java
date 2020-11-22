b499200d777c292b75e934a91bda21cf3281e8fc
mskim <mskim@vivans.net>
2020-11-22 19:36:14 +0900

    기능추가 / mclasscode이용 DiaryType Properties목록 조회 api 추가

--- src/main/java/com/kt/smartfarm/mapper/DiaryTypeMapper.java
+++ src/main/java/com/kt/smartfarm/mapper/DiaryTypeMapper.java
@@ -8,12 +8,14 @@ import egovframework.rte.psl.dataaccess.mapper.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 
 @Mapper("diaryTypeMapper")
 public interface DiaryTypeMapper {
     List<DiaryTypeVO> getDiaryTypeList(@Param(value = "gsmKey") Long gsmKey, @Param(value = "houseId") Long houseId, @Param(value = "userIdx") Long userIdx);
     List<DiaryTypePropertiesVO> getDiaryTypePropertyList(@Param(value = "diaryTypeId") Long diaryTypeId);
+    List<DiaryTypePropertiesVO> getDiaryTypePropertyList(Map param);
     List<DiaryTypeAbleHouseVO> getDiaryAbleHouseId(@Param(value = "gsmKey") Long gsmKey, @Param(value = "diaryTypeId") Long diaryTypeId);
 
 }
